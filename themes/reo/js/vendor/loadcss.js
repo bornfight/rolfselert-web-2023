8cf65f39aba85eb55b5df5b8a981b8e815715c79
Luka Uzel <luka.uzel@bornfight.com>
2023-04-11 13:44:39 +0200

    i js vendor nestao zbog ignore

---
+++ themes/reo/js/vendor/loadcss.js
@@ -0,0 +1,38 @@
+(function(w){
+  "use strict";
+  /* exported loadCSS */
+  w.loadCSS = function( href, before, media ){
+    var doc = w.document;
+    var ss = doc.createElement( "link" );
+    var ref;
+    if( before ){
+      ref = before;
+    }
+    else {
+      var refs = ( doc.body || doc.getElementsByTagName( "head" )[ 0 ] ).childNodes;
+      ref = refs[ refs.length - 1];
+    }
+    var sheets = doc.styleSheets;
+    ss.rel = "stylesheet";
+    ss.href = href;
+    ss.media = "only x";
+    ref.parentNode.insertBefore( ss, ( before ? ref : ref.nextSibling ) );
+    var onloadcssdefined = function( cb ){
+      var resolvedHref = ss.href;
+      var i = sheets.length;
+      while( i-- ){
+        if( sheets[ i ].href === resolvedHref ){
+          return cb();
+        }
+      }
+      setTimeout(function() {
+        onloadcssdefined( cb );
+      });
+    };
+    ss.onloadcssdefined = onloadcssdefined;
+    onloadcssdefined(function() {
+      ss.media = media || "all";
+    });
+    return ss;
+  };
+}(this));
